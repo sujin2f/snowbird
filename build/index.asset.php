0f6eb290dfdbb120e144168a2604a5e3ffb9d484
Sujin <sujin.2f@gmail.com>
2026-09-17 13:50:04 -0400

    hero

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready'), 'version' => 'c4b3cae096fedf2b668b');
+<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready'), 'version' => '310de762fa7fa2aa704c');
--- patterns/hero.php
+++ patterns/hero.php
@@ -5,27 +5,42 @@
  * Categories: featured
  */
 ?>
-<!-- wp:cover {"overlayColor":"contrast","isUserOverlayColor":true,"align":"full","className":"hero__container"} -->
-<div class="wp-block-cover alignfull hero__container">
-	<span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-100 has-background-dim"></span>
-	<div class="wp-block-cover__inner-container">
-		
-		<!-- wp:heading {"textAlign":"center"} -->
-		<h2 class="wp-block-heading has-text-align-center"><?php esc_html_e( 'Canadians! Your Florida Real Estate Dreams, Simplified.', 'snowbird' ); ?></h2>
-		<!-- /wp:heading -->
 
-		<!-- wp:paragraph {"align":"center"} -->
-		<p class="has-text-align-center"><?php esc_html_e( 'This is my little home away from home.', 'snowbird' ); ?></p>
+<!-- wp:cover {
+	"dimRatio":40,
+	"className":"hero__wrapper"
+} -->
+<div class="wp-block-cover hero__wrapper">
+	<span aria-hidden="true" class="wp-block-cover__background has-background-dim-40 has-background-dim"></span>
+
+	<div class="wp-block-cover__inner-container">
+		<!-- wp:paragraph {"className":"badge"} -->
+		<p class="badge">100% Free Service · No Pressure · No Obligation</p>
 		<!-- /wp:paragraph -->
 
-		<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-		<div class="wp-block-buttons">
-			<!-- wp:button {"className":"wp-block-button is-style-outline"} -->
-			<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button"><?php esc_html_e( 'Button A', 'snowbird' ); ?></a></div>
-			<!-- /wp:button -->
-		</div>
-		<!-- /wp:buttons -->
+		<!-- wp:heading -->
+		<h2 class="wp-block-heading">Canadians! Your Florida Real Estate Dreams, Simplified.</h2>
+		<!-- /wp:heading -->
 
+		<!-- wp:paragraph {"className":"hero__description"} -->
+		<p class="hero__description">
+			For 35 years, I help Canadians Find, Buy, and Sell at the right price with the perfect LOCAL agent. Access ON & OFF Market properties cost-free.
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:button {"className":"button--lg button--primary"} -->
+		<div class="wp-block-button button--lg button--primary">
+			<a
+				class="wp-block-button__link wp-element-button"
+				href="https://realtyconnect.xyz"
+				target="_blank"
+				rel="noopener"
+			>
+
+				Find Your Local Agent 
+			</a>
+		</div>
+  		<!-- /wp:button -->
 	</div>
 </div>
 <!-- /wp:cover -->
