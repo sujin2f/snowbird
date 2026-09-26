e71b8c21338f0f49c1753877e77ccff7cf131c9d
Sujin <sujin.2f@gmail.com>
2026-09-26 11:15:37 -0400

    finalizw

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready'), 'version' => '0fe4b39c7e9ffd3cb1d1');
+<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready'), 'version' => '4270471af8815f39481a');
--- patterns/contact.php
+++ patterns/contact.php
@@ -13,8 +13,11 @@
         "className":"contact__container"
     } -->
     <div class="wp-block-columns contact__container">
-        <!-- wp:column {"width":"50%"} -->
-        <div class="wp-block-column" style="flex-basis:50%">
+        <!-- wp:column {
+            "width":"50%",
+            "className":"card card--transparent"
+        } -->
+        <div class="wp-block-column card card--transparent" style="flex-basis:50%">
             <!-- wp:heading -->
             <h2>Why use Realty Connect?</h2>
             <!-- /wp:heading -->
@@ -22,27 +25,58 @@
             <!-- wp:list -->
             <ul class="wp-block-list">
                 <!-- wp:list-item -->
-                <li>NETWORK OF 90,000 TOP-RATED AGENTS</li>
+                <li>
+                    <div>NETWORK OF 90,000 TOP-RATED AGENTS</div>
+                    <!-- wp:template-part {
+                        "slug":"icons-user-check",
+                        "tagName":"div",
+                        "className":"card__icon icon--box icon--ocean"
+                    } /-->
+                </li>
                 <!-- /wp:list-item -->
 
                 <!-- wp:list-item -->
-                <li>UNBIASED RECOMMENDATIONS</li>
+                <li>
+                    <div>UNBIASED RECOMMENDATIONS</div>
+                    <!-- wp:template-part {
+                        "slug":"icons-user-check",
+                        "tagName":"div",
+                        "className":"card__icon icon--box icon--ocean"
+                    } /-->
+                </li>
                 <!-- /wp:list-item -->
 
                 <!-- wp:list-item -->
-                <li>SERVING EVERY ZIP CODE IN ALL 50 STATES</li>
+                <li>
+                    <div>SERVING EVERY ZIP CODE IN ALL 50 STATES</div>
+                    <!-- wp:template-part {
+                        "slug":"icons-user-check",
+                        "tagName":"div",
+                        "className":"card__icon icon--box icon--ocean"
+                    } /-->
+                </li>
                 <!-- /wp:list-item -->
 
                 <!-- wp:list-item -->
-                <li>IT'S FAST, EASY & 100% FREE!</li>
+                <li>
+                    <div>IT'S FAST, EASY & 100% FREE!</div>
+                    <!-- wp:template-part {
+                        "slug":"icons-user-check",
+                        "tagName":"div",
+                        "className":"card__icon icon--box icon--ocean"
+                    } /-->
+                </li>
                 <!-- /wp:list-item -->
             </ul>
             <!-- /wp:list -->
         </div>
         <!-- /wp:column -->
 
-        <!-- wp:column {"width":"50%"} -->
-        <div class="wp-block-column" style="flex-basis:50%">
+        <!-- wp:column {
+            "width":"50%",
+            "className":"card card--transparent"
+        } -->
+        <div class="wp-block-column card card--transparent contact" style="flex-basis:50%">
             <!-- wp:contact-form-7/contact-form-selector -->
             <div class="wp-block-contact-form-7-contact-form-selector">[contact-form-7]</div>
             <!-- /wp:contact-form-7/contact-form-selector -->
